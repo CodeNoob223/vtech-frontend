d8203cb651031af4a63150dee0e5e5500ba1072f
Codenoob223 <vuongdepzai1262003@gmail.com>
2023-07-30 22:11:44 +0700

    commit

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,15 +2,10 @@ import { useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import SideBar from "../components/SideBar";
 import Header from "../components/Header";
-import MessageBox from "../components/Notification/MessageBox";
-import { useAppDispatch, useAppSelector } from "../app/hook";
-import { closeNotification } from "../features/pageNotification/pageNotificationSlice";
 import Footer from "../components/Footer";
 
 export default function Root() {
   const [isSideBarOpen, setIsSideBarOpen] = useState<boolean>(false);
-  const pageNotification = useAppSelector(state => state.pageNotification);
-  const dispatch = useAppDispatch();
 
   const location = useLocation();
 
@@ -24,12 +19,6 @@ export default function Root() {
         setIsOpen={setIsSideBarOpen}
         location={location.pathname}
       />
-      {pageNotification.show && 
-      <MessageBox 
-        content={pageNotification.message as string} 
-        messageType={pageNotification.type}
-        onClose={() => dispatch(closeNotification())} 
-      />}
       <main className="relative bg-black min-h-[100vh] h-auto w-full">
         <Outlet />
       </main>
