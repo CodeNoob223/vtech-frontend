cb086d0ecd8bc849d52deab1f110032a7afa2df1
Codenoob223 <vuongdepzai1262003@gmail.com>
2023-07-31 09:47:30 +0700

    change z index

--- src/components/NotificationsContainer.tsx
+++ src/components/NotificationsContainer.tsx
@@ -1,11 +1,10 @@
-import { useAppDispatch, useAppSelector } from "../app/hook"
-import { closeNotification } from "../features/pageNotification/pageNotificationSlice";
+import { useAppSelector } from "../app/hook"
 import MessageBox from "./Notification/MessageBox";
 
 export default function NotificationContainer() : JSX.Element {
   const notifications = useAppSelector(state => state.pageNotification);
 
-  return <div className="fixed z-50 w-max h-max top-10 right-10 flex flex-col gap-2">
+  return <div className="fixed z-100 w-max h-max top-10 right-10 flex flex-col gap-2">
     {notifications.map(notif => {
       return <MessageBox 
         key={notif.id || ""}
