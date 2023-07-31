837384394b601b05fc063adf1527cebf541ee722
Codenoob223 <vuongdepzai1262003@gmail.com>
2023-07-31 23:25:49 +0700

    Changing password!

--- src/routes/views/Account.tsx
+++ src/routes/views/Account.tsx
@@ -1,27 +1,54 @@
-import { useEffect, useState } from "react";
-import Button from "../../components/Button/Button";
-import getToken from "../../helpers/getLocalStorage";
-import SmallCard from "../../components/Card/SmallCard";
-import ButtonLink from "../../components/Button/ButtonLink";
+import { useState } from "react";
 import { Helmet } from "react-helmet";
 import { useAppDispatch, useAppSelector } from "../../app/hook";
-import { localhostIP } from "../../App";
-import { deleteRequest, getRequest } from "../../helpers/fetchData";
 import { updateNotification } from "../../features/pageNotification/pageNotificationSlice";
+import Button from "../../components/Button/Button";
+import { postRequest } from "../../helpers/fetchData";
+import { localhostIP } from "../../App";
+import getToken from "../../helpers/getLocalStorage";
+import { logOutAction } from "../../features/userData/userSlice";
 
 export default function EditAccount() {
     const user = useAppSelector(state => state.userData);
+    const [newPass, setNewPass] = useState<string>("");
+    const [type, setType] = useState<"text" | "password">("password");
 
     const dispatch = useAppDispatch()
 
-    const copyToClipboard = (value: string) => {
-        // Copy the text inside the text field
-        navigator.clipboard.writeText(value);
-        dispatch(updateNotification({
-            show: true,
-            type: "bg-success",
-            message: "Copied url to clipboard!"
-        }));
+    const sendRequest = async () => {
+        if (user._id === "") {
+            dispatch(updateNotification({
+                show: true,
+                type: "bg-error",
+                message: "Please login first!"
+            }));
+
+            setTimeout(() => {
+                dispatch(logOutAction());
+            }, 300);
+        };
+
+        if (newPass.length < 10) {
+            dispatch(updateNotification({
+                show: true,
+                message: "Password is too short!",
+                type: "bg-error"
+            }))
+        };
+
+        const res = await postRequest(`${localhostIP}/api/interact/otp`, await getToken("access"), {
+            request_user: user._id,
+            action: "change_password",
+            payload: newPass
+        });
+
+        if (res.success) {
+            dispatch(updateNotification({
+                show: true,
+                message: "Please confirm in your email!",
+                type: "bg-success"
+            }));
+        };
     }
 
     return (
@@ -35,15 +62,50 @@ export default function EditAccount() {
                     {/* BLOG CONTENT */}
                     <div className="absolute top-[-15px] sm:top-[-22px] left-7 sm:left-8 px-2 text-white bg-black">
                         <h1 className="text-[6vw] sm:text-[32px]">
-                            Edit your account
+                            Account settings
                         </h1>
                     </div>
 
-
-                    <div className="w-full px-2">
-                        <div className="w-full flex flex-row flex-shrink-0 flex-wrap gap-4">
-                            
+                    <div className="w-full">
+                        <p className="text-white01 mb-2">Change your password:</p>
+                        <div className="relative w-full max-w-[400px] h-[40px] mb-5">
+                            <input
+                                name="_id"
+                                autoComplete="off"
+                                type={type}
+                                className="w-full h-full rounded-sm pl-2"
+                                placeholder="New password"
+                                value={newPass}
+                                onChange={(e) => {
+                                    setNewPass(e.target.value);
+                                }}
+                            />
+                            <span
+                                className={`absolute medium-icon right-[16px] top-[8px] cursor-pointer ${type === "password" ? "icon-Visibility" : "icon-Visibility-off"} text-background opacity-25 hover:opacity-100`}
+                                onClick={() => {
+                                    setType(prev => {
+                                        if (prev === "password") return "text";
+                                        else return "password";
+                                    });
+                                }}
+                            ></span>
                         </div>
+                        {
+                            newPass.length > 9 ?
+                                <Button
+                                    content="Confirm"
+                                    myStyles="bg-success"
+                                    iconPos="back"
+                                    icon="icon-Lock"
+                                    handleClick={sendRequest}
+                                />
+                                : <Button
+                                    content="Confirm"
+                                    myStyles="bg-disable"
+                                    iconPos="back"
+                                    icon="icon-Lock"
+                                />
+                        }
                     </div>
                 </div>
             </div>
